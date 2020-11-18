78de1a9236689479a622a021cfb8ff4f0e252524
mithrax <rahul.anthonym@gmail.com>
2020-11-18 14:46:06 -0500

    compare user input password with stored password in db

--- models/User.js
+++ models/User.js
@@ -20,13 +20,27 @@ const UserSchema = new mongoose.Schema({
   todos: [{type: mongoose.Schema.Types.ObjectID, ref: 'Todo'}]
 })
 
+// password hash condition on user submission
 UserSchema.pre('save', function(next){
   if(!this.isModified('password'))
     return next()
     bcrypt.hash(this.password,10, (err, passwordHash)=> {
       if(err)
-      return next(err)
+        return next(err)
       this.password = passwordHash;
       next()
-    })
-})
+    });
+});
+
+// checking is user entered password matches password hash stored in db
+UserSchema.methods.comparePassword = function(password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch)=> {
+    if(err)
+      return console(err)
+    else {
+      if (!isMatch)
+        return cb(null, isMatch);
+      return cb(null, this)
+    }
+  })
+}
