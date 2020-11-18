7da87521028217551a971c257b86fda41a87d8f3
mithrax <rahul.anthonym@gmail.com>
2020-11-18 14:41:26 -0500

    add assword hash conditions on save

--- models/User.js
+++ models/User.js
@@ -20,3 +20,13 @@ const UserSchema = new mongoose.Schema({
   todos: [{type: mongoose.Schema.Types.ObjectID, ref: 'Todo'}]
 })
 
+UserSchema.pre('save', function(next){
+  if(!this.isModified('password'))
+    return next()
+    bcrypt.hash(this.password,10, (err, passwordHash)=> {
+      if(err)
+      return next(err)
+      this.password = passwordHash;
+      next()
+    })
+})
