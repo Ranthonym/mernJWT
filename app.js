7e95516553c9b36ddac8059f0a3849dc87786d29
mithrax <rahul.anthonym@gmail.com>
2020-11-18 15:00:38 -0500

    finish setup of User.js

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const User = require("./models/User");
 
 app.use(cookieParser());
 app.use(express.json());
 
 mongoose.connect(
-  "mongodb://lcalhost:27017/mernauth",
+  "mongodb://localhost:27017/mernauth",
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log("successfully connected to database");
   }
 );
+// const userInput = {
+//   username: "jak",
+//   password: "1234567",
+//   role: "admin",
+// };
+
+// const user = new User(userInput);
+// user.save((err, document) => {
+//   if (err) console.log(err);
+//   console.log(document);
+// });
 
 app.listen(5000, () => {
   console.log("express server started");
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-cons mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -14,33 +14,31 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['user', 'admin'],
-    required: true
+    enum: ["user", "admin"],
+    required: true,
   },
-  todos: [{type: mongoose.Schema.Types.ObjectID, ref: 'Todo'}]
-})
+  todos: [{ type: mongoose.Schema.Types.ObjectID, ref: "Todo" }],
+});
 
 // password hash condition on user submission
-UserSchema.pre('save', function(next){
-  if(!this.isModified('password'))
-    return next()
-    bcrypt.hash(this.password,10, (err, passwordHash)=> {
-      if(err)
-        return next(err)
-      this.password = passwordHash;
-      next()
-    });
+UserSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+  bcrypt.hash(this.password, 10, (err, passwordHash) => {
+    if (err) return next(err);
+    this.password = passwordHash;
+    next();
+  });
 });
 
 // checking is user entered password matches password hash stored in db
-UserSchema.methods.comparePassword = function(password, cb) {
-  bcrypt.compare(password, this.password, (err, isMatch)=> {
-    if(err)
-      return console(err)
+UserSchema.methods.comparePassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) return console(err);
     else {
-      if (!isMatch)
-        return cb(null, isMatch);
-      return cb(null, this)
+      if (!isMatch) return cb(null, isMatch);
+      return cb(null, this);
     }
-  })
-}
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
